6be2afdcb76cf0dbdd6f9509a583895a8adebea3
Roko <36516651+rperica@users.noreply.github.com>
2022-12-19 01:13:00 +0100

    removed extras

--- client/src/App.js
+++ client/src/App.js
@@ -3,48 +3,15 @@ import HomeScreen from "./pages/Home";
 import LoginForm from "./pages/Login";
 import RegisterForm from "./pages/Register";
 import './App.css'
-// import Axios from 'axios';
+
 function App()
 {
-//   const [usernameReg, setUsernameReg] = useState("");
-//   const [passwordReg, setPasswordReg] = useState("");
-
-//   const [usernameLog, setUsernameLog] = useState("");
-//   const [passwordLog, setPasswordLog] = useState("");
-
-//   const register=()=>{
-//     Axios.post("http://localhost:5000/register", {username: usernameReg, password: passwordReg});
-//   }
-
-//   const login=()=>{
-//     Axios.post("http://localhost:5000/login", {username: usernameLog, password: passwordLog});
-//   }
-
   return (
     <div className="componentContainer">
         <HomeScreen />
         <LoginForm />
         <RegisterForm />
     </div>
-    // <div>
-    //   <div>
-    //     <h1>Registration</h1>
-    //     <label>Username</label>
-    //     <input type="text" onChange={(e)=>{setUsernameReg(e.target.value);}}/>
-    //     <label>Register</label>
-    //     <input type="password" onChange={(e)=>{setPasswordReg(e.target.value);}}/>
-    //     <button onClick={register}>Register</button>
-    //   </div>
-
-    //   <div>
-    //     <h1>Login</h1>
-    //     <label>Username</label>
-    //     <input type="text" onChange={(e)=>{setUsernameLog(e.target.value);}} />
-    //     <label>Register</label>
-    //     <input type="password" onChange={(e)=>{setPasswordLog(e.target.value);}} />
-    //     <button onClick={login}>Login</button>
-    //   </div>
-    // </div>
   );
 }
 
